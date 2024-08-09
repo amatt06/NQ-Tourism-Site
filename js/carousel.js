6d71b1aca38a84acc069aeec04bc720cd76df56c
amatt06 <s3853376@student.rmit.edu.au>
2024-08-09 13:15:47 +1000

    responsive carousel

--- js/carousel.js
+++ js/carousel.js
@@ -7,6 +7,23 @@ const progressBar = document.querySelector('sl-progress-bar');
 
 let scrollPosition = 0;
 
+function getVisibleImages() {
+    if (window.innerWidth <= 480) {
+        return 1; // Mobile screens show 1 image
+    } else if (window.innerWidth <= 768) {
+        return 2; // Smaller screens show 2 images
+    } else {
+        return 3; // Desktop screens show 3 images
+    }
+}
+
+function updateCarousel() {
+    const visibleImages = getVisibleImages();
+    const imageWidth = gallery.children[0].offsetWidth;
+    gallery.style.transform = `translateX(-${scrollPosition * imageWidth}px)`;
+    progressBar.value = ((scrollPosition + visibleImages) / gallery.children.length) * 100;
+}
+
 prevButton.addEventListener('click', () => {
     if (scrollPosition > 0) {
         scrollPosition--;
@@ -15,14 +32,15 @@ prevButton.addEventListener('click', () => {
 });
 
 nextButton.addEventListener('click', () => {
-    if (scrollPosition < gallery.children.length - 3) {
+    const visibleImages = getVisibleImages();
+    if (scrollPosition < gallery.children.length - visibleImages) {
         scrollPosition++;
-        updateCarousel();
+    } else {
+        scrollPosition = 0; // Go back to the first image
     }
+    updateCarousel();
 });
 
-function updateCarousel() {
-    const imageWidth = gallery.children[0].offsetWidth;
-    gallery.style.transform = `translateX(-${scrollPosition * imageWidth}px)`;
-    progressBar.value = ((scrollPosition + 1) / (gallery.children.length - 2)) * 100;
-}
+window.addEventListener('resize', updateCarousel);
+
+updateCarousel();
